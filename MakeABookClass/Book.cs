5eab782f348024312ed355365d77bb3b10beeba4
Leah Kirk <leah.kirks@gmail.com>
2023-07-27 19:52:57 -0230

    Add project files.

---
+++ MakeABookClass/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace MakeABookClass;
+
+public record Book
+{
+    public required string BookTitle { get; set; }
+    public required string AuthorName { get; set; }
+    public required DateOnly ReleaseDate { get; set; }
+}
---
+++ MakeABookClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using MakeABookClass;
+using Microsoft.VisualBasic;
+/*
+Book book1 = new Book();
+
+book1.BookTitle = "Frankenstein";
+book1.AuthorName = "Mary Shelley";
+
+Console.WriteLine(book1.BookTitle + "\n" + book1.AuthorName);
+
+Console.WriteLine(book1.ReleaseDate);
+*/
+var book2 = new Book()
+{
+    BookTitle = "Frankensteiners",
+    AuthorName = "Married Shell",
+    ReleaseDate = DateOnly.FromDateTime(DateTime.Today)
+};
+
+
+Console.WriteLine(book2.ReleaseDate);
